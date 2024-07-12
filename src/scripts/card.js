1aa41ef857f013e5503c05e050ab13c3b18b760c
Vladimir <123962001+Muesliestein@users.noreply.github.com>
2024-07-12 19:19:34 +0300

    like button

--- src/scripts/card.js
+++ src/scripts/card.js
@@ -6,17 +6,33 @@ function deleteCard(evt) {
 }
 
 function toggleLike(evt) {
-  evt.target.classList.toggle('card__like_active');
+  const likeButton = evt.target;
+  const cardElement = likeButton.closest('.card');
+  const likeCountElement = cardElement.querySelector('.card__like_count');
+  
+  likeButton.classList.toggle('card__like_active');
+  
+  let likeCount = parseInt(likeCountElement.textContent);
+  
+  if (likeButton.classList.contains('card__like_active')) {
+    likeCount += 1;
+  } else {
+    likeCount -= 1;
+  }
+  
+  likeCountElement.textContent = likeCount;
 }
 
 function createCard(card, handleCardClick) {
   const cardContent = cardTemplate.cloneNode(true);
   const cardImage = cardContent.querySelector('.card__image');
   const cardName = cardContent.querySelector('.card__name');
+  const likeCountElement = cardContent.querySelector('.card__like_count');
 
   cardName.textContent = card.name;
   cardImage.alt = card.name;
   cardImage.src = card.link;
+  likeCountElement.textContent = card.likes.length; // Отображение количества лайков
 
   cardContent.getElementById('card_delete').addEventListener('click', deleteCard);
   cardContent.querySelector('.card__like').addEventListener('click', toggleLike);
--- src/scripts/validation.js
+++ src/scripts/validation.js
@@ -1,96 +1,95 @@
 const validationConfig = {
-    formSelector: '.popup__form',
-    inputSelector: '.popup__input',
-    submitButtonSelector: '.popup__save',
-    inactiveButtonClass: 'popup__save_disabled',
-    inputErrorClass: 'popup__input_type_error',
-    errorClass: 'popup__input-error',
-    namePattern: /^[a-zA-Zа-яА-ЯёЁ\s-]+$/,
-    urlPattern: /^(https?|chrome):\/\/[^\s$.?#].[^\s]*$/,
-  };
-  
-  const showInputError = (formElement, inputElement, errorMessage, config) => {
-    const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
-    inputElement.classList.add(config.inputErrorClass);
-    errorElement.textContent = errorMessage;
-    errorElement.classList.add(config.errorClass);
-  };
-  
-  const hideInputError = (formElement, inputElement, config) => {
-    const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
-    inputElement.classList.remove(config.inputErrorClass);
-    errorElement.classList.remove(config.errorClass);
-    errorElement.textContent = '';
-  };
-  
-  const isValid = (formElement, inputElement, config) => {
-    if (!inputElement.validity.valid) {
-      if (inputElement.validity.valueMissing) {
-        showInputError(formElement, inputElement, 'Вы пропустили это поле.', config);
-      } else if (inputElement.validity.tooShort || inputElement.validity.tooLong) {
-        showInputError(formElement, inputElement, inputElement.dataset.error, config);
-      } else if (inputElement.validity.patternMismatch) {
-        showInputError(formElement, inputElement, 'Разрешены только латинские, кириллические буквы, знаки дефиса и пробелы.', config);
-      } else if (inputElement.type === 'url' && !config.urlPattern.test(inputElement.value)) {
-        showInputError(formElement, inputElement, 'Введите адрес сайта.', config);
-      } else {
-        showInputError(formElement, inputElement, inputElement.validationMessage, config);
-      }
-    } else if (inputElement.type === 'text' && !config.namePattern.test(inputElement.value)) {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__save',
+  inactiveButtonClass: 'popup__save_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__input-error',
+  namePattern: /^[a-zA-Zа-яА-ЯёЁ\s-]+$/,
+  urlPattern: /^(https?|chrome):\/\/[^\s$.?#].[^\s]*$/,
+};
+
+const showInputError = (formElement, inputElement, errorMessage, config) => {
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+  inputElement.classList.add(config.inputErrorClass);
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add(config.errorClass);
+};
+
+const hideInputError = (formElement, inputElement, config) => {
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+  inputElement.classList.remove(config.inputErrorClass);
+  errorElement.classList.remove(config.errorClass);
+  errorElement.textContent = '';
+};
+
+const isValid = (formElement, inputElement, config) => {
+  if (!inputElement.validity.valid) {
+    if (inputElement.validity.valueMissing) {
+      showInputError(formElement, inputElement, 'Вы пропустили это поле.', config);
+    } else if (inputElement.validity.tooShort || inputElement.validity.tooLong) {
+      showInputError(formElement, inputElement, inputElement.dataset.error, config);
+    } else if (inputElement.validity.patternMismatch) {
       showInputError(formElement, inputElement, 'Разрешены только латинские, кириллические буквы, знаки дефиса и пробелы.', config);
     } else if (inputElement.type === 'url' && !config.urlPattern.test(inputElement.value)) {
       showInputError(formElement, inputElement, 'Введите адрес сайта.', config);
     } else {
-      hideInputError(formElement, inputElement, config);
+      showInputError(formElement, inputElement, inputElement.validationMessage, config);
     }
-  };
-  
-  const toggleButtonState = (inputList, buttonElement, config) => {
-    if (hasInvalidInput(inputList)) {
-      buttonElement.classList.add(config.inactiveButtonClass);
-      buttonElement.setAttribute('disabled', true);
-    } else {
-      buttonElement.classList.remove(config.inactiveButtonClass);
-      buttonElement.removeAttribute('disabled');
-    }
-  };
-  
-  const hasInvalidInput = (inputList) => {
-    return inputList.some((inputElement) => {
-      return !inputElement.validity.valid || 
-             (inputElement.type === 'text' && !validationConfig.namePattern.test(inputElement.value)) || 
-             (inputElement.type === 'url' && !validationConfig.urlPattern.test(inputElement.value));
+  } else if (inputElement.type === 'text' && !config.namePattern.test(inputElement.value)) {
+    showInputError(formElement, inputElement, 'Разрешены только латинские, кириллические буквы, знаки дефиса и пробелы.', config);
+  } else if (inputElement.type === 'url' && !config.urlPattern.test(inputElement.value)) {
+    showInputError(formElement, inputElement, 'Введите адрес сайта.', config);
+  } else {
+    hideInputError(formElement, inputElement, config);
+  }
+};
+
+const toggleButtonState = (inputList, buttonElement, config) => {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add(config.inactiveButtonClass);
+    buttonElement.setAttribute('disabled', true);
+  } else {
+    buttonElement.classList.remove(config.inactiveButtonClass);
+    buttonElement.removeAttribute('disabled');
+  }
+};
+
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid || 
+           (inputElement.type === 'text' && !validationConfig.namePattern.test(inputElement.value)) || 
+           (inputElement.type === 'url' && !validationConfig.urlPattern.test(inputElement.value));
+  });
+};
+
+const setEventListeners = (formElement, config) => {
+  const inputList = Array.from(formElement.querySelectorAll(config.inputSelector));
+  const buttonElement = formElement.querySelector(config.submitButtonSelector);
+  toggleButtonState(inputList, buttonElement, config);
+
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', () => {
+      isValid(formElement, inputElement, config);
+      toggleButtonState(inputList, buttonElement, config);
     });
-  };
-  
-  const setEventListeners = (formElement, config) => {
-    const inputList = Array.from(formElement.querySelectorAll(config.inputSelector));
-    const buttonElement = formElement.querySelector(config.submitButtonSelector);
-    toggleButtonState(inputList, buttonElement, config);
-  
-    inputList.forEach((inputElement) => {
-      inputElement.addEventListener('input', () => {
-        isValid(formElement, inputElement, config);
-        toggleButtonState(inputList, buttonElement, config);
-      });
-    });
-  };
-  
-  const enableValidation = (config) => {
-    const formList = Array.from(document.querySelectorAll(config.formSelector));
-    formList.forEach((formElement) => {
-      setEventListeners(formElement, config);
-    });
-  };
-  
-  const clearValidation = (formElement, config) => {
-    const inputList = Array.from(formElement.querySelectorAll(config.inputSelector));
-    const buttonElement = formElement.querySelector(config.submitButtonSelector);
-    inputList.forEach((inputElement) => {
-      hideInputError(formElement, inputElement, config);
-    });
-    toggleButtonState(inputList, buttonElement, config);
-  };
-  
-  export { enableValidation, clearValidation, validationConfig };
-  
+  });
+};
+
+const enableValidation = (config) => {
+  const formList = Array.from(document.querySelectorAll(config.formSelector));
+  formList.forEach((formElement) => {
+    setEventListeners(formElement, config);
+  });
+};
+
+const clearValidation = (formElement, config) => {
+  const inputList = Array.from(formElement.querySelectorAll(config.inputSelector));
+  const buttonElement = formElement.querySelector(config.submitButtonSelector);
+  inputList.forEach((inputElement) => {
+    hideInputError(formElement, inputElement, config);
+  });
+  toggleButtonState(inputList, buttonElement, config);
+};
+
+export { enableValidation, clearValidation, validationConfig };
